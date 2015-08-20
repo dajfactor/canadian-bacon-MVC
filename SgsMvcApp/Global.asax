ec96b3bf57a1ded5210f92f9d86a800f102d2331
Justin Lai <dajfactor@gmail.com>
2015-08-19 22:52:39 -0700

    Initial app

---
+++ SgsMvcApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SgsMvcApp.MvcApplication" Language="C#" %>
---
+++ SgsMvcApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SgsMvcApp.Startup))]
+namespace SgsMvcApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
